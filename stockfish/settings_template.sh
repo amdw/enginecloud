e769c04f5a7dc87df1d77691c8244676692a8b08
Andrew Medworth <github@medworth.org.uk>
2022-04-24 23:42:57 +0100

    Upgrade to Stockfish 15 (closes #6) (#7)
    Add ssh.sh and git checkout enginecloud for convenience.

--- stockfish/settings_template.sh
+++ stockfish/settings_template.sh
@@ -1,10 +1,12 @@
 EC_HOME=$HOME/enginecloud
 SSH_USER=$USER
-STOCKFISH_VERSION=stockfish_14.1_linux_x64_bmi2
+STOCKFISH_VERSION=stockfish_15_linux_x64_bmi2
+STOCKFISH_BINARY=stockfish_15_x64_bmi2
 
 GCP_PROJECT=
 GCP_ZONE=
 GCP_MACHINE_TYPE=
-GCP_IMAGE_PROJECT=debian-cloud
-GCP_IMAGE_FAMILY=debian-10
+# Stockfish 15 binary requires at least libc 2.34
+GCP_IMAGE_PROJECT=ubuntu-os-cloud
+GCP_IMAGE_FAMILY=ubuntu-2204-lts
 GCP_INSTANCE_NAME=stockfish
