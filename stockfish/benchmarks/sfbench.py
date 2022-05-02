8d40c20ebcd252826b7a6c8db109dbc4652604f4
Andrew Medworth <github@medworth.org.uk>
2022-05-02 22:21:26 +0100

    Enable benchmarking of hash table size

--- stockfish/benchmarks/sfbench.py
+++ stockfish/benchmarks/sfbench.py
@@ -18,6 +18,7 @@ import collections
 import csv
 from dataclasses import dataclass
 import itertools
+import operator
 import re
 import statistics
 import subprocess
@@ -40,8 +41,6 @@ class SeriesParams:
     repetitions: int
     # Number of consecutive times we should tolerate a failure to improve on the previous best performance
     max_failures_to_improve: int
-    # Function to allow us to force continuing to the next BenchParams even if we would otherwise stop
-    force_continue: Callable[[BenchParams], bool]
 
 
 def run_benchmark(stockfish_binary: str, params: BenchParams) -> Mapping[str, int]:
@@ -66,6 +65,7 @@ def run_benchmark(stockfish_binary: str, params: BenchParams) -> Mapping[str, in
 def run_series(
     stockfish_binary: str,
     series_params: SeriesParams,
+    force_continue: Callable[[BenchParams], bool],
     params_seq: Iterable[BenchParams],
 ) -> Mapping[BenchParams, Sequence[int]]:
     results = collections.defaultdict(list)
@@ -83,7 +83,7 @@ def run_series(
         else:
             failures_to_improve += 1
             print(f'Average {average:,.1f} is not an improvement on best {best_average:,.1f}: failures={failures_to_improve}', file=sys.stderr)
-            if not series_params.force_continue(params) and failures_to_improve >= series_params.max_failures_to_improve:
+            if not force_continue(params) and failures_to_improve >= series_params.max_failures_to_improve:
                 break
 
     return results
@@ -93,31 +93,46 @@ def run_varying_threads(
     stockfish_binary: str,
     depth: int,
     tt_size_mb: int,
-    repetitions: int,
-    max_failures_to_improve: int,
+    series_params: SeriesParams,
     min_final_threads: int,
 ) -> Mapping[BenchParams, Sequence[int]]:
     def force_continue(params: BenchParams):
         return params.threads < min_final_threads
 
-    series_params = SeriesParams(
-        repetitions=repetitions,
-        max_failures_to_improve=max_failures_to_improve,
-        force_continue=force_continue,
-    )
+    thread_counts = itertools.count(start=1, step=1)
     return run_series(
         stockfish_binary,
         series_params,
-        (BenchParams(threads=t, tt_size_mb=tt_size_mb, depth=depth) for t in itertools.count(start=1, step=1)),
+        force_continue,
+        (BenchParams(threads=t, tt_size_mb=tt_size_mb, depth=depth) for t in thread_counts),
+    )
+
+
+def run_varying_ttsize(
+    stockfish_binary: str,
+    depth: int,
+    threads: int,
+    series_params: SeriesParams,
+) -> Mapping[BenchParams, Sequence[int]]:
+    # Start with 1024Mb and increase by 2x each time
+    hash_sizes = itertools.accumulate(itertools.repeat(2), func=operator.mul, initial=1024)
+    return run_series(
+        stockfish_binary,
+        series_params,
+        lambda p: False,
+        (BenchParams(threads=threads, tt_size_mb=size, depth=depth) for size in hash_sizes),
     )
 
 
 def print_results(machine_type: str, results: Mapping[BenchParams, Sequence[int]]):
     w = csv.writer(sys.stdout)
+    header = ['MachineType', 'Threads', 'TTSizeMb', 'Depth', 'MeanNPS']
+    header.extend(f'Run{i}NPS' for i in range(1, len(next(iter(results.values())))+1))
+    w.writerow(header)
     for (params, nps_results) in sorted(results.items()):
         row = [machine_type, params.threads, params.tt_size_mb, params.depth]
-        row.extend(nps_results)
         row.append(statistics.mean(nps_results))
+        row.extend(nps_results)
         w.writerow(row)
 
 
@@ -133,15 +148,29 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('stockfish_binary', default='/tmp/stockfish/stockfish', nargs='?')
     parser.add_argument('--depth', type=int, default=14)
+    parser.add_argument('--threads', type=int, default=0)
     parser.add_argument('--tt_size_mb', type=int, default=16)
+    parser.add_argument('--test_varying', type=str, choices=['threads', 'ttsize'], default='threads')
     parser.add_argument('--repetitions', type=int, default=3)
     parser.add_argument('--max_failures_to_improve', type=int, default=3)
+    parser.add_argument('--machine_type', type=str, default='',
+        help='Override machine type; leave blank to query from metadata server')
     args = parser.parse_args()
-    machine_type = get_machine_type()
+
+    machine_type = args.machine_type if args.machine_type else get_machine_type()
     machtype_cpus = int(machine_type.split('-')[-1])
-    results = run_varying_threads(
-        args.stockfish_binary, args.depth, args.tt_size_mb,
-        args.repetitions, args.max_failures_to_improve, machtype_cpus)
+    series_params = SeriesParams(repetitions=args.repetitions, max_failures_to_improve=args.max_failures_to_improve)
+    if args.test_varying == 'threads':
+        results = run_varying_threads(
+            args.stockfish_binary, args.depth, args.tt_size_mb,
+            series_params, machtype_cpus)
+    elif args.test_varying == 'ttsize':
+        threads = args.threads if args.threads >= 1 else machtype_cpus
+        results = run_varying_ttsize(
+            args.stockfish_binary, args.depth, threads, series_params)
+    else:
+        raise ValueError(f'Unsupported test_varying {args.test_varying}')
+
     print_results(machine_type, results)
 
 
