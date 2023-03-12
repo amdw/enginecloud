1bebb7215d31d5b5931b24a0044b34235defd84f
Andrew Medworth <github@medworth.org.uk>
2023-03-12 22:04:35 +0000

    Change Stockfish settings template to 15.1

--- stockfish/settings_template.sh
+++ stockfish/settings_template.sh
@@ -1,12 +1,12 @@
 EC_HOME=$HOME/enginecloud
 SSH_USER=$USER
-STOCKFISH_VERSION=stockfish_15_linux_x64_bmi2
-STOCKFISH_BINARY=stockfish_15_x64_bmi2
+STOCKFISH_VERSION="stockfish_15.1_linux_x64_bmi2"
+STOCKFISH_BINARY="stockfish_15.1_x64_bmi2"
 
 GCP_PROJECT=
 GCP_ZONE=
 GCP_MACHINE_TYPE=
-# Stockfish 15 binary requires at least libc 2.34
+# Newer Stockfish binaries require newer libc versions hence relatively recent Ubuntu
 GCP_IMAGE_PROJECT=ubuntu-os-cloud
 GCP_IMAGE_FAMILY=ubuntu-2204-lts
 GCP_INSTANCE_NAME=stockfish
