4a3fc2f455b140959af3db0ee03564e1d41b8180
Andrew Medworth <github@medworth.org.uk>
2026-03-13 21:23:37 +0000

    Update date

--- stockfish/benchmarks/sfbench.py
+++ stockfish/benchmarks/sfbench.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2022-2024 Andrew Medworth
+# Copyright 2022-2024, 2026 Andrew Medworth
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
